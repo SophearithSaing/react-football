839f836690de74f76edc501eaf6e7083daca84fd
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2022-02-19 22:42:44 +0700

    Change website title and favicon

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import Matches from './Components/Matches';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Untitled Football Project</h1>
       <Calendar />
       <h2>Matches</h2>
       <Matches />
+      <div className='attribution'>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          href='https://icons8.com/icon/GTsE9Lh1DNKA/football'
+        >
+          Football
+        </a>{' '}
+        icon by{' '}
+        <a target='_blank' rel='noreferrer' href='https://icons8.com'>
+          Icons8
+        </a>
+      </div>
     </div>
   );
 }
